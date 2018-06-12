f13714720e2970ccd3848cd57f62b5e2ec0b7941
Park June Chul <pjc0247@naver.com>
2018-06-13 03:32:22 +0900

    m

---
+++ src/minivm.core/Exception/VMRuntimeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minivm
+{
+    public class VMRuntimeException : Exception
+    {
+        public VMRuntimeException(string msg) : base(msg)
+        {
+        }
+    }
+}
